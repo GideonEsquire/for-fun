c75b98f2fc115f394620fd8062c53bf2a24751d5
Gideon <GideonEsquire@gmail.com>
2022-04-02 12:53:51 -0700

    not working locally

---
+++ forNicki/README.md
@@ -0,0 +1,34 @@
+Wager
+=====
+
+Short on money but still want to fuel your crippling gambling addiction?
+Worry not. With Wager(trademark) you can still be a broke degenerate.
+
+Pick from a database of non monetary stakes.
+
+Possible Features
+-----------------
+
+ - User able to filter possible outcomes
+ - Events that have a duration could have a variable time frame
+ - Bonuses for the winner
+ - Penalties for the loser
+ - Categories
+    1. Naughty
+    2. Housework
+        -Kitchen
+        -Cat box
+        -Vacuum
+        -Wipe tables
+        -Make Bed
+    5. Workout
+        -Push ups
+        -Wall sit
+        -Plank
+        -Situps
+    6. To the winner goes the spoils
+        -Control What's on tv
+    7. Humiliate the loser
+        -Embarrassing FB profile pic
+        -Can't wear pants
+
---
+++ forNicki/sketch.js
@@ -0,0 +1,43 @@
+// Vast majortiy copied from Daniel Shiffman
+// http://codingtra.in
+// Steering Text Paths
+// Video: https://www.youtube.com/watch?v=4hA7G3gup-4
+
+var font;
+var vehicles = [];
+
+function preload() {
+  font = loadFont('AvenirNextLTPro-Demi.otf');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+
+  // message = ' Happy Spring Nicole '
+  message = 'Happy Spring Nicole'
+
+  var points = font.textToPoints(message, 100, 200, 192, {
+    sampleFactor: 0.25
+  });
+
+  for (var i = 0; i < points.length; i++) {
+    var pt = points[i];
+    var vehicle = new Vehicle(pt.x, pt.y);
+    vehicles.push(vehicle);
+    // stroke(255);
+    // strokeWeight(8);
+    // point(pt.x, pt.y);
+  }
+}
+
+function draw() {
+  background(255);
+  for (var i = 0; i < vehicles.length; i++) {
+    var v = vehicles[i];
+    v.behaviors();
+    v.update();
+    v.show();
+  }
+}
+
