b61ed8194c63dff6d39c82bd7eb32b7ee1c80c19
Gideon <GideonEsquire@gmail.com>
2022-04-06 18:39:42 -0700

    added slider variables, just without the sliders

--- dzzz/particle.js
+++ dzzz/particle.js
@@ -2,8 +2,10 @@ function Particle(x, y) {
   this.pos = createVector(x, y);
   this.prev = createVector(x, y);
   this.vel = createVector();
-  // this.vel = p5.Vector.random2D();
   this.acc = createVector();
+  rand1 = Math.random() * 100
+  rand2 = Math.random() * 5000
+  rand3 = Math.random() * 3000
 
   this.update = function() {
     this.pos.add(this.vel);
@@ -13,7 +15,7 @@ function Particle(x, y) {
   }
 
   this.show = function() {
-    stroke(255, 10);
+    stroke(0, 0, 200, 10);
     strokeWeight(1);
     line(this.pos.x, this.pos.y, this.prev.x, this.prev.y);
     this.prev.x = this.pos.x;
@@ -22,10 +24,8 @@ function Particle(x, y) {
 
   this.attracted = function(target) {
     let force = p5.Vector.sub(target, this.pos);
-    // let dsquared = constrain(force.magSq(), 5, 100);
-    // let g = 30;
-    // let strength = g/dsquared;
-    let strength = 5;
+    let dsquared = constrain(force.magSq(), rand1, rand2);
+    let strength = rand3/dsquared;
     force.setMag(strength);
     this.acc.add(force);
   }
--- dzzz/sketch.js
+++ dzzz/sketch.js
@@ -1,23 +1,31 @@
-// var attractors = [];
+var attractors = [];
 var particles = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(0);
+  for (let i = 0; i < 7; i++) {
+    attractors.push(createVector(windowWidth*Math.random(), windowHeight*Math.random()));
+  }
 
-  for (let i = 0; i < windowWidth; i+=10) {
-    particles.push(new Particle(i,-1));
-    particles.push(new Particle(i,windowHeight+1));
+  for (let i = 0; i < windowWidth; i+=2) {
+    particles.push(new Particle(i, windowHeight));
+    particles.push(new Particle(i, -1));
+  }
+
+  for (let i = 0; i < windowHeight; i+=2) {
+    particles.push(new Particle(-1, i));
+    particles.push(new Particle(windowWidth, i));
   }
 
 }
 
 function draw() {
-
   for (let i = 0; i < particles.length; i++) {
-    particles[i].attracted(createVector(mouseX, mouseY));
+    for (let j = 0; j < attractors.length; j++) {
+      particles[i].attracted(attractors[j]);
+    }
     particles[i].update();
     particles[i].show();
   }
-  background(51, 10);
-
 }
